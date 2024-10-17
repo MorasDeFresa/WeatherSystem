75b37bf360420c8701499a8d0b637e5fe2eeaeed
MorasDeFresa <bencito10@gmail.com>
2024-10-17 11:09:51 -0500

    Circuit - ADD (WiFi Connection)

--- Circuit/ESP32/ESP32.ino
+++ Circuit/ESP32/ESP32.ino
@@ -2,23 +2,37 @@
 #include "WiFi.h"
 #include <WiFiClient.h>
 
+IPAddress ESP32_IP;
+
 void setup() {
   Serial.begin(115200);
   WifiConnection();
+  delay(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
 
-char WifiConnection()
+void VerifyConnection(){
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.println("----------------------------------");
+    Serial.print("Successful connection SSID:");
+    Serial.println(WIFI_SSID);
+    Serial.print("IP ESP32: ");
+    Serial.println(ESP32_IP);
+    Serial.println("----------------------------------");
+  }
+}
+
+void WifiConnection()
 {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  IPAddress myIP = WiFi.localIP();
-  return myIP;
+  Serial.println(".");
+  ESP32_IP = WiFi.localIP();
+  VerifyConnection();
 }
